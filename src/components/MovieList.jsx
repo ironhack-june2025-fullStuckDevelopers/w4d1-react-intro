d927fca1e5bb5d1ff311d7b2beeb5a7db9dd091e
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-07-08 14:18:51 +0200

    fix: unique key

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,14 @@
 import moviesArr from "../data/movies.json"
 
 function MovieList(){
-
     return (
         <section id="movie-list">
 
             <h3>List of movies:</h3>
 
-            {moviesArr.map((movieObj, i, arr) => {
+            {moviesArr.map((movieObj) => {
                 return (
-                    <div className="box">
+                    <div key={movieObj.id} className="box">
                         <p>{movieObj.title}</p>
                         <p>{movieObj.year}</p>
                     </div>
