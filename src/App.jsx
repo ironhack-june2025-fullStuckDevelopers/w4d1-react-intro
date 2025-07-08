55c3a2b88c0c0e35a869d2b18908bfc2e15f296b
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-07-08 12:56:39 +0200

    add example of map()

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header.jsx"
 import MyFirstComponent from "./components/MyFirstComponent.jsx"
 import Footer from "./components/Footer.jsx"
 import User from "./components/User.jsx"
+import MovieList from "./components/MovieList.jsx"
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <User firstName="Alice" surname="Smith" />
       <User firstName="Bob" surname="Brown" />
       <User firstName="Charlie" surname="Doe" />
+
+      <MovieList />
 
       <MyFirstComponent />
 
