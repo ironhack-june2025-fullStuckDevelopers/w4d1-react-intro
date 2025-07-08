a1508b1dc7f0c2b8cec83491f4d926ca4da79d2b
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-07-08 14:52:27 +0200

    add examples of conditional rendering

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,33 @@
 import moviesArr from "../data/movies.json"
 
 function MovieList(){
+
+    let message = "";
+    if(moviesArr.length > 0){
+        message = <h3>Number of Movies: {moviesArr.length}</h3>
+    } else {
+        message = <h3>Sorry, no movies to display</h3>
+    }
+
     return (
         <section id="movie-list">
 
-            <h3>List of movies:</h3>
+            {message}
 
             {moviesArr.map((movieObj) => {
                 return (
                     <div key={movieObj.id} className="box">
                         <p>{movieObj.title}</p>
-                        <p>{movieObj.year}</p>
+                        <p>Year: {movieObj.year}</p>
+                        <p>Rating: {movieObj.rating}</p>
+
+                        { movieObj.imgURL
+                            ? <img src={movieObj.imgURL} alt="Movie Porter" /> 
+                            : <img src="https://dummyimage.com/182x268/ffffff/000000" alt="Movie Porter" /> 
+                        }
+
+                        {movieObj.rating > 7 && <p className="badge">TOP CHOICE</p>}
+
                     </div>
                 )
             })}
